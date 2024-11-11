4bf85ab1a825a19d1762c897141787b771ef590f
Isaac Kellogg <keldtdev@gmail.com>
2024-11-11 11:38:10 -0500

    Initial: Copy project from OpenDI repo

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export class Config {
+    //Canvas size
+    static paperWidth = "64%"; //See style.css for .right, which is taking up the other 34%.
+    static paperHeight = 900;
+
+    //Used for resizing elements
+    static minElementWidth = 120;
+    static maxElementWidth = 200;
+    static minElementHeight = 60;
+    //Do not limit height, so longer text can fully render
+}
